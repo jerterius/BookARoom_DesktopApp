68fa8059c568d3915f8f3a383656fc172788dbe3
MAdersson <36193435+MAdersson@users.noreply.github.com>
2018-02-12 13:19:57 +0100

    Ändring
    Lagt till metoder i Dalen

--- BookARoom/BookARooom/DAL/DAL.cs
+++ BookARoom/BookARooom/DAL/DAL.cs
@@ -11,19 +11,37 @@ namespace BookARoom.DAL
 {
     public class Dal
     {
-       
-            
-       /* public void Connect()
-        { 
-            using (var db = new BookingContext())
-            {
-            }
-        } */
+        SqlDataAdapter da = new SqlDataAdapter();
 
-            public void Add(object addToTable)
-            {
-            using (var db = new BookingContext())
-            {
+        public static SqlConnection OpenConnect()
+         { 
+            string dbAdress = "Data Source = MSSQLLocalDB;Initial Catalog=BookingDB;Integrated Security=SSPI";
+            
+            SqlConnection con = new SqlConnection(dbAdress);
+            con.Open();
+            return con;
+         } 
+
+        public static void CloseConnect(SqlConnection con)
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+                con.Close();
+            con.Dispose();
+        }
+
+        public SqlCommand QueryStatement(string query)
+        {
+            SqlCommand cmd = new SqlCommand(query, OpenConnect());
+            cmd.CommandType = CommandType.Text;
+            da.SelectCommand = cmd;
+
+            return cmd;
+        }
+
+        public void Add(object addToTable)
+        {
+            OpenConnect();
+            var db = new BookingContext();
 
                 switch (addToTable)
                 {
@@ -48,59 +66,53 @@ namespace BookARoom.DAL
                         db.SaveChanges();
                         break;
                 }
-            }
-            }
+            CloseConnect(OpenConnect());
+        }
 
 
-        
-            public int TotalPrice(string bookingNumber)
+        public int TotalPrice(string bookingNumber)
+        {
+            int totalPrice = 0;
+            using (var db = new BookingContext())
             {
-                int totalPrice = 0;
+                var query = from b in db.Bookings
+                            where b.BookingNumber.Equals(bookingNumber)
+                            select b;
 
-                using (var db = new BookingContext())
+                foreach (var item in query)
                 {
-                    var query = from b in db.Bookings
-                                where b.BookingNumber.Equals(bookingNumber)
-                                select b;
-
-                    foreach (var item in query)
-                    {
-                         totalPrice += item.Room.Price;
-                    }
-
-                    return totalPrice;
+                    totalPrice += item.Room.Price;
                 }
+
+            CloseConnect(OpenConnect());
+            return totalPrice;
             }
-
-
+        }
 
         public DataTable FindAvailableRooms(City city)
         {
             DataTable dt = new DataTable();
 
-            SqlConnection con = new SqlConnection("Data Source = MSSQLLocalDB;Initial Catalog=BookingDB;Integrated Security=SSPI");
-            SqlCommand cmd = new SqlCommand("select * from Rooms " +
+            OpenConnect();
+            QueryStatement("select * from Rooms " +
                 "inner join Hotels on Hotel.Adress = Room.Adress " +
                 "inner join City where City.CityName = Hotel.CityName" +
-                "and where Booking.RoomNumber != Roomnumber", con);
-            cmd.CommandType = CommandType.Text;
-            SqlDataAdapter da = new SqlDataAdapter();
-            da.SelectCommand = cmd;
+                "and where Booking.RoomNumber != Roomnumber");
+            
             da.Fill(dt);
 
+            CloseConnect(OpenConnect());
             return dt;
         }
             
-            public bool DeleteBooking(string bookingNumber)
-            {
-                SqlConnection con = new SqlConnection("Data Source = MSSQLLocalDB;Initial Catalog=BookingDB;Integrated Security=SSPI");
-                SqlCommand cmd = new SqlCommand("delete from booking where bookingNumber=?", con);
-                cmd.CommandType = CommandType.Text;
-                SqlDataAdapter da = new SqlDataAdapter();
-                da.SelectCommand = cmd;
+        public bool DeleteBooking(string bookingNumber)
+        {
+            OpenConnect();
+            SqlCommand cmd = QueryStatement("delete from booking where bookingNumber=?");
+            CloseConnect(OpenConnect());
 
-                return (0 != cmd.ExecuteNonQuery());
-            }
+            return (0 != cmd.ExecuteNonQuery());
+        }
 
      }
  }
