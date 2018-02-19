2a39ec2ae4d4909696dd4b78db88852308f409e1
MAdersson <36193435+MAdersson@users.noreply.github.com>
2018-02-19 12:13:24 +0100

    Changes in Dal
    Ändrat i metoden GetHotelsWIthAvailableRooms

--- BookARoom/BookARooom/DAL/DAL.cs
+++ BookARoom/BookARooom/DAL/DAL.cs
@@ -135,13 +135,7 @@ namespace BookARoom.DAL
             var hotelList = from h in db.Hotels
                             join b in db.Bookings on h.Adress equals b.Adress
                             join d in dates on b.Date equals d
-                            where !dates.Any(b => b.Date == d)
-
-                            where h.CityName == cityName && h.Countryname == countryName 
-
-                            
-                            join r in db.Rooms on h.Adress equals r.Adress
-                            where !db.Bookings.Any(b => b.RoomNumber == r.RoomNumber)
+                            where b.Date != d
                             select h;
 
             return hotelList.Distinct().ToList();
