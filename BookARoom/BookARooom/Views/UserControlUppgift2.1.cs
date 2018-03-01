6149214b754939c7387b0a725587dd513e61c229
nmicin <35593969+nmicin@users.noreply.github.com>
2018-03-01 12:13:37 +0100

    UC för uppgift2.1

---
+++ BookARoom/BookARooom/Views/UserControlUppgift2.1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BookARoom.ServiceReferenceFile;
+using Uppgift2._1; 
+
+namespace BookARoom.Views
+{
+    public partial class UserControlUppgift2 : UserControl
+    {
+        ControllerUppgift2 controller = new ControllerUppgift2();
+
+        public UserControlUppgift2()
+        {
+            InitializeComponent();
+            cbSelectFile.Text = "Select....";
+        }
+
+        private void cbSelectFile_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+                
+            
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            
+                rtbShowFile.Text = controller.GetFile();
+            
+
+        }
+
+        private void lbSelectFile_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BookARoom/Uppgift2.1/Controller/ControllerUppgift2.1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Uppgift2._1
+{
+    public class ControllerUppgift2
+    {
+        WebServiceUppgift2__1 ws = new WebServiceUppgift2__1();
+
+        public string GetFile()
+        {
+            return ws.GetFile();
+        }
+    }
+}
