c446fad73ed9a8ed87cf4b74e580ea55111190be
nmicin <35593969+nmicin@users.noreply.github.com>
2018-03-01 16:30:08 +0100

    UC Uppgift2.1

---
+++ BookARoom/BookARooom/Views/UCUppgift2_1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BookARoom.ServiceReferenceUppg2_1;
+
+namespace BookARoom.Views
+{
+    public partial class UCUppgift2_1 : UserControl
+    {
+        
+        public UCUppgift2_1()
+        {
+            InitializeComponent();
+            cbSelectFile.Text = "Select...";
+        }
+
+        
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbSelectFile_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using(WSUppgift2SoapClient client = new WSUppgift2SoapClient())
+            {
+                textBoxShowFile.Text = client.GetFile();
+            }
+
+        }
+    }
+}
